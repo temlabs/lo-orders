99a102b42549bea36e892a025ce7d6f535e753ec
temlabs <temilabinjo@hotmail.co.uk>
2024-04-23 02:06:04 +0100

    scroll to search bar on focus

--- src/components/searchModal/SearchBar.tsx
+++ src/components/searchModal/SearchBar.tsx
@@ -1,15 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { SearchIcon } from "../icons/SearchIcon";
 
 export function SearchBar() {
   const [searchTerm, setSearchTerm] = useState<string>();
+  const searchBarRef = useRef<HTMLInputElement>(null);
+
+  const scrollToInput = () => {
+    searchBarRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
 
   return (
     <div className="flex flex-row items-center">
       <SearchIcon color="black" className="-mr-7" />
       <input
+        ref={searchBarRef}
+        onFocus={scrollToInput}
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         type={"text"}
